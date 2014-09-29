70c6257cc35a2ad567ff89f82876c2033e589713
YY <yanyu0517@126.com>
2014-09-29 11:48:22 +0800

    dom操作提交
    增加了一些dom操作

--- src/base/dom.js
+++ src/base/dom.js
@@ -201,5 +201,150 @@ define(function(require) {
      */
     dom.attr = $dom.attr;
 
+    /**
+     * 获得/设置元素的位置
+     *
+     * @param {HTMLElement} elm 目标元素
+     * @param {Object} pos 位置数据
+     *
+     *     @example
+     *     dom.offset(element); //get
+     *     dom.offset(element, {top: 10, left: 30}); //set
+     */
+    dom.offset = function(element, pos) {
+        var offset = {
+            left: 0,
+            top: 0
+        }
+        if(!element){
+            return offset;
+        }
+        //如果传了pos，则认为是需要设置offset
+        if(pos && pos !== void 0){
+            dom.setOffset(element, pos);
+        }
+        var doc = element.ownerDocument,
+            win,
+            docElem;
+        if(!doc){
+            return;
+        }
+        docElem = doc.documentElement;
+        if (typeof element.getBoundingClientRect !== 'undefined') {
+            offset = element.getBoundingClientRect();
+        }
+        win = doc.defaultView;
+        return {
+            top: offset.top  + (win.pageYOffset || docElem.scrollTop)  - (docElem.clientTop  || 0),
+            left: offset.left + (win.pageXOffset || docElem.scrollLeft) - (docElem.clientLeft || 0)
+        };
+    };
+
+    /**
+     * 设置元素的位置
+     *
+     * @param {HTMLElement} element 目标元素
+     * @param {Object} pos 位置数据
+     *
+     *     @example
+     *     dom.setOffset(element, {top: 10, left: 30});
+     */
+    dom.setOffset = function(element, pos) {
+        if(!element){
+            return;
+        }
+        var curPosition, curLeft, curCSSTop, curTop, curOffset, curCSSLeft, calculatePosition,
+            position = dom.css(element, "position"),
+            props = {};
+        if (position === "static") {
+            element.style.position = "relative";
+        }
+        curOffset = dom.offset(element);
+        curCSSTop = dom.css(element, "top");
+        curCSSLeft = dom.css(element, "left");
+        //position是经过计算得到的
+        calculatePosition = (position === "absolute" || position === "fixed") && (_.indexOf([curCSSTop, curCSSLeft], "auto") > -1);
+        if (calculatePosition) {
+            curPosition = dom.position(element);
+            curTop = curPosition.top;
+            curLeft = curPosition.left;
+        } else {
+            curTop = parseFloat(curCSSTop) || 0;
+            curLeft = parseFloat(curCSSLeft) || 0;
+        }
+        if (pos.top != null) {
+            props.top = ((pos.top - curOffset.top) + curTop) + 'px';
+        }
+        if (pos.left != null) {
+            props.left = ((pos.left - curOffset.left) + curLeft) + 'px';
+        }
+        dom.css(element, props);
+    }
+
+    /**
+     * 获得相对于offsetParent的定位数据
+     *
+     * @param {HTMLElement} element 目标元素
+     * @return {top: , left: }
+     *
+     *     @example
+     *     dom.position(element);
+     */
+    dom.position = function(element) {
+        if(!element){
+            return;
+        }
+        var offsetParent, offset,
+            parentOffset = {top: 0, left: 0};
+
+        //如果元素的position是fixed，window是元素节点的offsetParent
+        //parentOffset = {top:0, left: 0}
+        if (dom.css(element, "position") === "fixed") {
+            if (typeof element.getBoundingClientRect !== 'undefined') {
+                offset = element.getBoundingClientRect();
+            } else {
+                offset = { top: 0, left: 0 };
+            }
+        } else {
+            offsetParent = dom.offsetParent(element);
+
+            offset = dom.offset(element);
+            if(offsetParent.nodeName.toLowerCase() !== "html"){
+                parentOffset = offsetParent.offset();
+            }
+            //增加border的宽度
+            parentOffset.top  += dom.css(offsetParent, "borderTopWidth");
+            parentOffset.left += dom.css(offsetParent, "borderLeftWidth");
+        }
+
+        //计算的时候需要减去父节点的offset和自身的margin
+        return {
+            top:  offset.top  - parentOffset.top - dom.css(element, "marginTop"),
+            left: offset.left - parentOffset.left - dom.css(element, "marginLeft")
+        };
+
+    }
+
+    /**
+     * 获得最近的定位父节点
+     *
+     * @param {HTMLElement} element 目标元素
+     * @return {HTMLElement || documentElement}
+     *
+     *     @example
+     *     dom.offsetParent(element);
+     */
+    dom.offsetParent = function(element) {
+        if(!element){
+            return;
+        }
+        var offsetParent = element.offsetParent || window.document.documentElement;
+
+        while (offsetParent && (offsetParent.nodeName.toLowerCase() !== "html" && dom.css( offsetParent, "position" ) === "static" ) ) {
+            offsetParent = offsetParent.offsetParent;
+        }
+        return offsetParent || window.document.documentElement;;
+    }
+
     return dom;
 });
